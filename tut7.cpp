9a2f1355aaec3156fa42efb58d4d8710f377e79d
iamhasrh1818 <iamhasrh1818@gmail.com>
2026-08-09 18:08:11 +0530

    Added new C++ programs

--- tut7.cpp
+++ tut7.cpp
@@ -5,6 +5,8 @@ using namespace std;
 int c=645;
 
 int main(){
+    // built in datatypes
+
 int a,b,c;
 cout<<"the value of a is "<<endl;
 cin>>a;
@@ -14,6 +16,8 @@ c=a+b;
 cout<<"the sum is equal to "<<c<<endl;
 cout<<"the global sum is "<<::c<<endl;
 
+// literals
+
 float d=43.4;
 long double e=43.4;
 cout<<"the value of d is "<<d<<endl<<"the value of e is "<<e<<endl;
@@ -22,6 +26,22 @@ cout<<"the size of 43.4f is "<<sizeof(43.4f)<<endl;
 cout<<"the size of 43.4F is "<<sizeof(43.4F)<<endl;
 cout<<"the size of 43.4l is "<<sizeof(43.4l)<<endl;
 cout<<"the size of 43.4L is "<<sizeof(43.4L)<<endl;
+
+// referecnce variable
+
+float x=457;
+float & y=x;
+cout<<x<<endl;
+cout<<y<<endl;
+
+// type casting
+
+int z=46;
+float l=45.46;
+cout<<"the value of z is "<<z<<endl;
+cout<<"the value of l is "<<l<<endl;
+cout<<"the value of l is "<<(int)l<<endl;
+cout<<"the value of l is "<<int(l)<<endl;
 return 0;
 }
 
