3d585c63e9edba27d1edc6ba2a1d8a13ba21d504
iamhasrh1818 <iamhasrh1818@gmail.com>
2026-08-14 22:15:36 +0530

    uploaded

---
+++ loops.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+
+using namespace std;
+int main(){
+
+    // FOR LOOP
+
+     int i;
+     for(i=0;i<=40;i++){
+         cout<<i<<endl;
+     }
+
+    // WHILE LOOP
+
+     int y=0;
+     while(y<=10){
+         cout<<y<<endl;
+         y++;
+     }
+
+    // DO WHILE LOOP
+
+     int z=0;
+     do{
+         cout<<z<<endl;
+         z++;
+     }while(z<=10);
+
+// TABLE OF 6
+
+int a=6,b;
+for(b=1;b<=10;b++){
+    cout<<a*b<<endl;
+}
+
+
+    return 0;
+}
