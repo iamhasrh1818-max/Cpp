863eeb509cfd57a2f1322e08857c0495c6515e2c
iamhasrh1818 <iamhasrh1818@gmail.com>
2026-08-24 00:39:05 +0530

    uploaded

---
+++ enum.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+    enum meal{breakfast,lunch,dinner};
+    meal m1=lunch;
+    cout<<(m1==2)<<endl;
+
+     cout<<m1<<endl;
+    
+    
+     cout<<breakfast;
+     cout<<lunch;
+     cout<<dinner;
+
+
+return 0;
+}
