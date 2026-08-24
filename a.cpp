b71cef6575f70113c3b091e1e534b9b019773352
iamhasrh1818 <iamhasrh1818@gmail.com>
2026-08-24 14:46:20 +0530

    uploaded

---
+++ a.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+
+int main(){
+cout<<setw(3)<<'*'<<endl;
+cout<<setw(2)<<"**";
+return 0;
+}
---
+++ triangle.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+
+int main(){
+    
+    cout<<setw(6)<<'*'<<endl;
+    cout<<setw(5)<<'*';
+    cout<<setw(2)<<'*'<<endl;
+    cout<<setw(4)<<'*';
+    cout<<setw(4)<<'*'<<endl;
+    cout<<setw(3)<<'*';
+    cout<<setw(6)<<'*'<<endl;
+    cout<<setw(2)<<'*';
+    cout<<setw(8)<<'*'<<endl;
+    cout<<setw(1)<<'*';
+    cout<<setw(3)<<'*';
+    cout<<setw(4)<<'*';
+    cout<<setw(3)<<'*'<<endl;
+    
+    
+    
+
+
+
+return 0;
+}
