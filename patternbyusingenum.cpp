e8ccbba69d99708562d5e716815f55c736d256fe
iamhasrh1818 <iamhasrh1818@gmail.com>
2026-08-24 14:07:32 +0530

    uploaded

---
+++ patternbyusingenum.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    enum gym{chest,triceps,back,biceps,shoulders,core,legs};
+    cout<<chest<<endl;
+    cout<<chest;
+    cout<<triceps<<endl;
+    cout<<chest;
+    cout<<triceps;
+    cout<<back<<endl;
+    cout<<chest;
+    cout<<triceps;
+    cout<<back;
+    cout<<biceps<<endl;
+    cout<<chest;
+    cout<<triceps;
+    cout<<back;
+    cout<<biceps;
+    cout<<shoulders<<endl;
+    cout<<chest;
+    cout<<triceps;
+    cout<<back;
+    cout<<biceps;
+    cout<<shoulders;
+    cout<<core<<endl;
+    cout<<chest;
+    cout<<triceps;
+    cout<<back;
+    cout<<biceps;
+    cout<<shoulders;
+    cout<<core;
+    cout<<legs<<endl;
+return 0;
+}
