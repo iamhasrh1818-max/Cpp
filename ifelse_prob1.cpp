39633a8a44ec7a79c8af352a9c3af50898949d9e
iamhasrh1818 <iamhasrh1818@gmail.com>
2026-08-29 22:13:32 +0530

    uploaded

---
+++ ifelse_prob1.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a,b,c;
+    cout<<"Enter the three numbers "<<endl;
+    cin>>a>>b>>c;
+    if((a>b)&&(a>c)){
+        cout<<"a is greater"<<endl;
+    }
+    else if((b>a)&&(b>c)){
+        cout<<"b is greater"<<endl;
+    }
+    else{
+        cout<<"c is greater";
+    }
+
+return 0;
+}
