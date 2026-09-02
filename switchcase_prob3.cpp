6c11895e64b3b068d3974e10f6373a919a087af5
iamhasrh1818 <iamhasrh1818@gmail.com>
2026-09-02 13:56:42 +0530

    uploaded

--- switchcase_prob3.cpp
+++ switchcase_prob3.cpp
@@ -5,10 +5,29 @@ int main(){
     int num;
     cout<<"Enter the number "<<endl;
     cin>>num;
-    switch(num){
-        case ():
+    if(num<0&&num>100){
+        cout<<"Invalid number ";
+        
+    }
+   int category=num/10;
+
+    switch(category){
+        case 10:
+        case 9:
         cout<<"Rank A";
         break;
+        case 8:
+        cout<<"Rank B";
+        break;
+        case 7:
+        cout<<"Rank c";
+        break;
+        case 6:
+        cout<<"Rank D";
+        break;
+        default:
+        cout<<"Rank F";
+        
     }
 
 return 0;
